a6d970168559436ce6a745f3422de4b9fa563521
akansha_tayal <akansha_tayal@excellencetechnologies.in>
2018-06-28 14:06:33 +0530

    Done The Formating

--- js/main.js
+++ js/main.js
@@ -39,38 +39,45 @@ $(document).ready(function() {
 
       $("#tdatas").append(tdata);
     });
-    $('th').each(function(col) {
-      $(this).click(function() {
-      if ($(this).is('.asc')) {
-      $(this).removeClass('asc');
-      $(this).addClass('desc selected');
-      sortOrder = -1;
-      }
-      else {
-      $(this).addClass('asc selected');
-      $(this).removeClass('desc');
-      sortOrder = 1;
-      }
-      $(this).siblings().removeClass('asc selected');
-      $(this).siblings().removeClass('desc selected');
-      var arrData = $('table').find('tbody>tr:has(td)').get();
-      // console.log(arrData);
-      arrData.sort(function(a, b) {
-      var val1 = $(a).children('td').eq(col).text().toUpperCase();
-      // console.log(val1);
-      var val2 = $(b).children('td').eq(col).text().toUpperCase();
-      // console.log(val2);
-      if($.isNumeric(val1) && $.isNumeric(val2)){
-      return sortOrder == 1 ? val1-val2 : val2-val1;
-      }
-      else{
-      return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
-      }
-      });
-     $.each(arrData, function(index, row) {
-     $('tbody').append(row);
+  $('th').each(function(col)
+  {
+      $(this).click(function()
+      {
+          if ($(this).is('.asc'))
+          {
+            $(this).removeClass('asc');
+            $(this).addClass('desc selected');
+            sortOrder = -1;
+          }
+          else
+          {
+            $(this).addClass('asc selected');
+            $(this).removeClass('desc');
+            sortOrder = 1;
+          }
+        $(this).siblings().removeClass('asc selected');
+        $(this).siblings().removeClass('desc selected');
+        var arrData = $('table').find('tbody>tr:has(td)').get();
+        // console.log(arrData);
+        arrData.sort(function(a, b)
+        {
+          var val1 = $(a).children('td').eq(col).text().toUpperCase();
+          // console.log(val1);
+          var val2 = $(b).children('td').eq(col).text().toUpperCase();
+          // console.log(val2);
+          if($.isNumeric(val1) && $.isNumeric(val2))
+          {
+            return sortOrder == 1 ? val1-val2 : val2-val1;
+          }
+          else
+          {
+            return (val1 < val2) ? -sortOrder : (val1 > val2) ? sortOrder : 0;
+          }
+        });
+        $.each(arrData, function(index, row)
+        {
+          $('tbody').append(row);
+        });
      });
-    });
   });
-
 });
